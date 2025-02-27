4a33d281843a822cbc22aad52b34757d38d925eb
haider-sulman <hs.vains@gmail.com>
2025-02-27 13:29:26 +0500

    Add files via upload

---
+++ assingment lab 1 - Copy.py
@@ -0,0 +1,29 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:44:40) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print(1,2,3,4,5,6,7)
+1 2 3 4 5 6 7
+>>> x=64+32
+>>> print(x)
+96
+>>> x=2
+>>> y=8
+>>> sum=x+y
+>>> print(sum)
+10
+>>> print("today is","26/2/2025")
+today is 26/2/2025
+>>> 
+>>> ================================ RESTART ================================
+>>> phone=input("enter your phone number")
+Traceback (most recent call last):
+  File "C:\Users\sp24-bba-117\Documents\tt.py", line 1, in <module>
+    phone_number=int(input('03214633828'))
+ValueError: invalid literal for int() with base 10: 'phone=input("enter your phone number")'
+>>> phone=input("enter your phone number")
+enter your phone number 03214633828
+>>> print("your phone number is",phone)
+your phone number is  03214633828
+>>> s="lucky"
+>>> print(s)
+lucky
+>>> 
